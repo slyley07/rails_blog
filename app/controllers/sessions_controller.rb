907ed2738c4be88ac86e347382fdea882474323e
mamosken <marcelamosken@gmail.com>
2015-05-07 13:54:24 -0400

    fixed bug in session controller, add a for to log in close, and treste sigup and sigin page close #9 close #13

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 
   def create
   	@user=User.find_by(username: params[:username])
-  	if @user and user.password == params[:password]
-  		session[:user_id] = @user.user_id
+  	if @user and @user.password == params[:password]
+  		session[:user_id] = @user.id
   		flash[:notice]= "Succesfully logged in."
   		redirect_to @user
   end
