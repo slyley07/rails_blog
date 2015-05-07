c523c6cb94d63ae051add0ed0fee4ac6d475f20a
mamosken <marcelamosken@gmail.com>
2015-05-07 12:00:11 -0400

    created action in session controller, signin and sign out, modified routes for sessions, and  insertes link to nav bar in application

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
 resources :users
   
   resources :posts , shallow: true, only:[:create, :new, :show, :destroy]  do
-     resources :comments, shallow: true, only:[:new, :create, :destroy]
+     resources :comments, only:[:new, :create, :destroy]
    end
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
