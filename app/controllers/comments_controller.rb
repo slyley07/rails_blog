f4250bc03bffd0b5a96253d335432b8b35044ea7
Bradley <showalter.bradley@gmail.com>
2015-05-12 10:27:18 -0400

    Fix minor routing bugs. Still need to fix comments. Posts link directly to a post page now.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   def create
+  	@post = Post.find_by(post_id: params[:post_id])
   	@comment = Comment.create(params.require(:comment).permit(:body).merge(user: current_user, post_id: params[:post_id]))
-  	redirect_to @comment.post
+  	redirect_to post_path(params[:post_id])
   end
 
   def destroy
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,9 @@ class PostsController < ApplicationController
   end
 
   def show
-  	@user = User.find_by(user_id: params[:user_id])
-  	@comment = Comment.new
-  	@post_com = @post.comments
+    # @user = User.find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments.all
   end
 
   def edit
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   def create
   	@post = Post.new(post_params)
   	if @post.save
-  		redirect_to user_posts_path
+  		redirect_to posts_path
   	else
   		render :new
   	end
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
   def update
   	if @post.update(post_params)
-  		redirect_to user_posts_path
+  		redirect_to posts_path
   	else
   		render :edit
   	end
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
   def destroy
   	@post.destroy
   	#comment destroy too
-  	redirect_to user_posts_path
+  	redirect_to posts_path
   end
 
   private
